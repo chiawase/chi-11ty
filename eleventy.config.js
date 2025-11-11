c24609e2906757ae1dda13b44ebf8579e72a3aea
Chi Señires <chi.senires@gmail.com>
2025-11-11 19:33:53 +0800

    Move the marking of draft posts in a way that works for me
    Not adding it directly to the title instead, but just appending it since
    "draft" is a variable I can check for anyways

--- eleventy.config.js
+++ eleventy.config.js
@@ -26,9 +26,9 @@ export default async function(eleventyConfig) {
 	
 	// Drafts, see also _data/eleventyDataSchema.js
 	eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
-		if (data.draft) {
-			data.title = `(draft) ${data.title}`;
-		}
+		// if (data.draft) {
+		// 	data.title = `(draft) ${data.title}`;
+		// }
 
 		if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
 			return false;
